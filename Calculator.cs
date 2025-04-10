f9b117fa1dc5dce5e42ed62a9bf4c46ed8b1240c
Mathilde <you@example.com>
2025-04-10 11:30:54 +0200

    xUnit

---
+++ Calculator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class Calculator : ICalculator
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public double? Divide(int a, int b)
+        {
+            if (b == 0 && a == 0)
+            {
+                return null;
+            }
+
+            return a / b;
+        }
+
+        public int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+
+        public int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+
+       
+
+    }
+}
